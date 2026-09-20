97ae87b2cbb052caffddfc1b743fc0e53a2ed67f
Alexey <vlad.shabalin.1992@mail.ru>
2026-09-20 23:07:18 +0300

    take from dmui determ loc game

--- Source/Host/meDMUIClient.cpp
+++ Source/Host/meDMUIClient.cpp
@@ -1,5 +1,9 @@
 #include "meDMUIClient.h"
+#include "../meUtils.h"
 #include "../mePluginInfo.h"
+
+#include <RE/S/Setting.h>
+
 
 static dmui::localize::LocalizeString lsGeneralCategory("$GeneralCategory", "General");
 static dmui::localize::LocalizeString lsCheatsCategory("$CheatsCategory", "Cheats");
@@ -9,6 +13,8 @@ static dmui::localize::LocalizeString lsBasketPage("$BasketPage", "Basket");
 static dmui::localize::LocalizeString lsGeneralPageSummary("$GeneralPageSummary", "Number of mods, and items that can be obtained, etc.");
 static dmui::localize::LocalizeString lsExplorerPageSummary("$CheatsPageSummary", "Searching for and receiving items.");
 static dmui::localize::LocalizeString lsBasketPageSummary("$BasketPageSummary", "Your shopping basket.");
+
+using namespace std::literals;
 
 void meDMUIClient::RendererGeneralPage() noexcept
 {
@@ -34,22 +40,52 @@ bool meDMUIClient::Connect() noexcept
 		kClientIcon);
 	if (!client)
 	{
-		REX::ERROR("meDMUIClient::Connect() Internal error");
+		REX::ERROR("meDMUIClient::Connect() Internal error"sv);
 		return false;
 	}
 	
 	if (!client->Connect())
 	{
 		if (!client->HostPresent())
-			REX::WARN("meDMUIClient::Connect() No dmui host is loaded; no menu this session");
+			REX::WARN("meDMUIClient::Connect() No dmui host is loaded; no menu this session"sv);
 		else
-			REX::ERROR("meDMUIClient::Connect() dmui registration failed, {}", DMUI_ResultToString(client->LastResult()));
+			REX::ERROR("meDMUIClient::Connect() dmui registration failed, {}"sv, DMUI_ResultToString(client->LastResult()));
 
 		return false;
 	}
 
+	std::string lfile = meUtils::GetRuntimeDirectory() + "Data/F4SE/Plugins/DearModdingUI/Translation/Mod Explorer/ModExplorer.txt";
+
+	// Retrieve the global collection of INI settings
+	auto settings = RE::INISettingCollection::GetSingleton();
+	if (!settings)
+	{
+		REX::ERROR("RE::INISettingCollection::GetSingleton return nullptr"sv);
+		return false;
+	}
+
+	// Look up the SLanguage:General setting
+	// Yeah, exactly SLanguage:General this Bethesda
+	auto setting = settings->GetSetting("SLanguage:General");
+	if (setting && (setting->GetType() == RE::Setting::SETTING_TYPE::kString))
+	{
+		std::string lang = setting->GetString().data();
+		lang.insert(0, "_");
+
+		auto it = lfile.find_last_of('.');
+		if (it != std::string::npos)
+			lfile.insert(it, lang);
+		else
+			lfile += lang;
+	}
+	else
+	{
+		REX::ERROR("RE::INISettingCollection::GetSetting no found \"SLanguage:General\" setting"sv);
+		return false;
+	}
+
 	auto localizeStringManager = dmui::localize::LocalizationManager::GetSingleton();
-	localizeStringManager->Init("Data/F4SE/Plugins/DearModdingUI/Translation/Mod Explorer/ModExplorer.txt", true);
+	localizeStringManager->Init(lfile);
 	if (localizeStringManager->Exists())
 		localizeStringManager->Load();
 
@@ -60,7 +96,7 @@ bool meDMUIClient::Connect() noexcept
 		.iconName = "info",
 		}) && (DMUI_RESULT_DUPLICATE_CATEGORY_ID != client->LastResult()))
 	{
-		REX::ERROR("meDMUIClient::Connect() dmui add category failed, {}", DMUI_ResultToString(client->LastResult()));
+		REX::ERROR("meDMUIClient::Connect() dmui add category failed, {}"sv, DMUI_ResultToString(client->LastResult()));
 		return false;
 	}
 
@@ -71,7 +107,7 @@ bool meDMUIClient::Connect() noexcept
 		.iconName = "bug-beetle",
 		}) && (DMUI_RESULT_DUPLICATE_CATEGORY_ID != client->LastResult()))
 	{
-		REX::ERROR("meDMUIClient::Connect() dmui add category failed, {}", DMUI_ResultToString(client->LastResult()));
+		REX::ERROR("meDMUIClient::Connect() dmui add category failed, {}"sv, DMUI_ResultToString(client->LastResult()));
 		return false;
 	}
 
@@ -83,7 +119,7 @@ bool meDMUIClient::Connect() noexcept
 		.iconName = "gear", },
 		std::addressof(RendererGeneralPage)) && (DMUI_RESULT_DUPLICATE_PAGE_ID != client->LastResult()))
 	{
-		REX::ERROR("meDMUIClient::Connect() Page 'General' registration failed, {}", DMUI_ResultToString(client->LastResult()));
+		REX::ERROR("meDMUIClient::Connect() Page 'General' registration failed, {}"sv, DMUI_ResultToString(client->LastResult()));
 		return false;
 	}
 
@@ -96,7 +132,7 @@ bool meDMUIClient::Connect() noexcept
 		.iconName = "hand-deposit", },
 		std::addressof(RendererExplorerPage)) && (DMUI_RESULT_DUPLICATE_PAGE_ID != client->LastResult()))
 	{
-		REX::ERROR("meDMUIClient::Connect() Page 'Explorer' registration failed, {}", DMUI_ResultToString(client->LastResult()));
+		REX::ERROR("meDMUIClient::Connect() Page 'Explorer' registration failed, {}"sv, DMUI_ResultToString(client->LastResult()));
 		return false;
 	}
 
@@ -109,11 +145,11 @@ bool meDMUIClient::Connect() noexcept
 		.iconName = "basket", },
 		std::addressof(RendererExplorerPage)) && (DMUI_RESULT_DUPLICATE_PAGE_ID != client->LastResult()))
 	{
-		REX::ERROR("meDMUIClient::Connect() Page 'Basket' registration failed, {}", DMUI_ResultToString(client->LastResult()));
+		REX::ERROR("meDMUIClient::Connect() Page 'Basket' registration failed, {}"sv, DMUI_ResultToString(client->LastResult()));
 		return false;
 	}
 
-	REX::INFO("meDMUIClient::Connect() Registered as '{}' with the dmui host", kClientId);
+	REX::INFO("meDMUIClient::Connect() Registered as '{}' with the dmui host"sv, kClientId);
 
 	return true;
 }
